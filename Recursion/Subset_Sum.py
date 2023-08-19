b47cd940510c4965c78ddbbd0953c5b159870af6
Junaid Zaidi <junaidzaidi@Junaids-MacBook-Pro.local>
2023-08-18 17:41:27 -0700

    Solved Subset_Sum

---
+++ Recursion/Subset_Sum.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+'''
+Subset Sum - Geeks for Geeks
+https://practice.geeksforgeeks.org/problems/subset-sums2234/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+'''
+
+
+def subsetSums(arr):
+    res = []
+    subsetSumUtil(arr, 0, 0, res)
+    res.sort()
+    return res
+
+def subsetSumUtil(arr, currentIndex, currentSum, res):
+    if currentIndex == len(arr):
+        # Once current index reaches out of bound append the result.
+        res.append(currentSum)
+        return
+    
+    # Picking up the element
+    subsetSumUtil(arr, currentIndex+1, currentSum, res)
+    
+    # Not picking up the element
+    subsetSumUtil(arr, currentIndex+1, currentSum+arr[currentIndex], res)
+
+
+print(subsetSums([5, 2, 1]))
