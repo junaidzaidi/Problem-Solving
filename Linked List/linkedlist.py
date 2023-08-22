68e9281f0c222c315359049f5c29189c2d907d3e
Junaid Zaidi <junaid_masroor@hotmail.com>
2023-08-21 19:11:24 -0700

    Solved Merge Two Sorted List

--- Linked List/linkedlist.py
+++ Linked List/linkedlist.py
@@ -29,3 +29,55 @@ def printLinkedListRecursive(head):
     printLinkedListRecursive(head.next)
 
 printLinkedListRecursive(first)
+
+
+def mergeTwoLists(self, list1, list2):
+    '''
+        Create a dummy list
+
+        Have seperate pointers on list1 and list2
+        Compare both pointers
+        Add the mimimum to dummy list
+
+        move dummy list pointer
+        move minimum list pointer
+
+        check if list1 is there append it to dummy list
+        check if list 2 is there append it to dummy list
+        
+    '''
+    dummyList = Node(-1)
+    dummyHead = dummyList
+
+    while list1 and list2:
+        minNode = None
+        if list1.value < list2.value:
+            minNode = list1
+            list1 = list1.next
+
+        else:
+            minNode = list2
+            list2 = list2.next
+        
+        dummyList.next = minNode
+        dummyList = dummyList.next
+
+    if list1:
+        dummyList.next = list1
+    
+    if list2:
+        dummyList.next = list2
+
+    return dummyHead.next
+
+    '''
+        -1, 1, 1,
+                |
+
+        1,2,3
+            |
+
+        1,3,4
+            |
+    '''
+    
